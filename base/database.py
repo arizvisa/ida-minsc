arizvisa/ida-minsc

commit e07f273a0966e98a2e9fc348b89b2a461c8672f0

base/database: fixed one of the variations of address.iterate to not tamper with the specified bounds before they're actually used

base/database.py
@@ -1721,19 +1721,18 @@ class address(object):
     @classmethod
     def iterate(cls, start, end):
         '''Iterate from address `start` to `end`.'''
-        start, end = builtins.map(interface.address.head, (start, end))
+        start, end = interface.address.within(start, end)
         step = cls.prev if start > end else cls.next
         return cls.iterate(start, end, step)
     @utils.multicase(start=six.integer_types, end=six.integer_types, step=callable)
     @classmethod
     def iterate(cls, start, end, step):
         '''Iterate from address `start` to `end` using the callable `step` to determine the next address.'''
-        start, end = builtins.map(interface.address.head, (start, end))
+        start, end = interface.address.inside(start, end)
         left, right = config.bounds()
-        right = idaapi.prev_not_tail(right)
 
         if start == end: return
-        op = operator.ge if start >= end else operator.lt
+        op = operator.lt if start < end else operator.ge
 
         res = start
         while res not in {idaapi.BADADDR, None} and left <= res < right and op(res, end):

commit 653d89c47dafb7d95b106fb4cb6c048d0ab6c18a

base/database: commit e07f273 broke address.iterate() as reported by @taodaqiao in #19. this commit wraps it in an exception handler that traps out-of-bounds errors

base/database.py
@@ -1734,11 +1734,12 @@ class address(object):
         if start == end: return
         op = operator.lt if start < end else operator.ge
 
-        res = start
-        while res not in {idaapi.BADADDR, None} and left <= res < right and op(res, end):
-            yield res
-            res = step(res)
-        return
+        try:
+            res = start
+            while res not in {idaapi.BADADDR, None} and left <= res < right and op(res, end):
+                yield res
+                res = step(res)
+        except E.OutOfBoundsError: pass
 
     @classmethod
     @utils.multicase(end=six.integer_types)