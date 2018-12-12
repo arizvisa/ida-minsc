d6edf5688c5c837faef5b0e35aae568cb82d8d3c
Ali Rizvi-Santiago <arizvisa@gmail.com>
2018-12-11 21:17:37 -0600

    internal/interface: added a `string` namespace for converting unicode strings back and forth from IDA's UTF-8.

--- base/_interface.py
+++ base/_interface.py
@@ -1248,3 +1248,20 @@ class bounds_t(namedtypedtuple):
     """
     _fields = ('left', 'right')
     _types = (six.integer_types, six.integer_types)
+
+class string(object):
+    """
+    IDA takes ascii strings and internally encodes them as UTF8. So
+    this class aims to normalize all of these strings by converting
+    them into a `unicode` type.
+    """
+
+    @classmethod
+    def of(cls, string):
+        '''Return a string from IDA in a format that is consistent'''
+        return None if string is None else string.decode('utf8') if isinstance(string, str) else string
+
+    @classmethod
+    def to(cls, string):
+        '''Convert a string into a form that IDA will accept.'''
+        return None if string is None else string.encode('utf8') if isinstance(string, unicode) else string
