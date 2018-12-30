8e27e640eea9935523967c636f640e5c17acd343
Ali Rizvi-Santiago <arizvisa@gmail.com>
2018-12-30 00:17:05 -0600

    internal/utils: fixed some bugs in `character.unescape` due to mistakenly inverted cases and bad copy-pasta.

--- base/_utils.py
+++ base/_utils.py
@@ -1211,8 +1211,8 @@ class character(object):
                 # check if the 'x' prefix is specified, which represents a hex digit
                 elif t == 'x':
                     hb, lb = (yield), (yield)
-                    if any(map(cls.hexQ, (hb, lb))):
-                        raise internal.exceptions.InvalidFormatError(u"{:s}.unescape({!s}) : Expected the next two characters ('{:s}', '{:s}') to be hex digits for an ascii character.".format('.'.join(('internal', __name__, cls.__name__)), iterable, internal.interface.string.escape(hb, '\''), internal.interface.string.escape(lb, '\'')))
+                    if any(not cls.hexQ(b) for b in {hb, lb}):
+                        raise internal.exceptions.InvalidFormatError(u"{:s}.unescape({!s}) : Expected the next two characters ('{:s}', '{:s}') to be hex digits for an ascii character.".format('.'.join(('internal', __name__, cls.__name__)), result, internal.interface.string.escape(hb, '\''), internal.interface.string.escape(lb, '\'')))
 
                     # convert the two hex digits into their integral forms
                     h, l = map(cls.of_hex, (hb.lower(), lb.lower()))
@@ -1226,8 +1226,8 @@ class character(object):
                 # if we find a 'u' prefix, then we have a unicode character
                 elif t == 'u':
                     hwb, lwb, hb, lb = (yield), (yield), (yield), (yield)
-                    if any(map(cls.hexQ, (hwb, lwb, hb, lb))):
-                        raise internal.exceptions.InvalidFormatError(u"{:s}.unescape({!s}) : Expected the next four characters ('{:s}', '{:s}', '{:s}', '{:s}') to be hex digits for a unicode character.".format('.'.join(('internal', __name__, cls.__name__)), iterable, internal.interface.string.escape(hwb, '\''), internal.interface.string.escape(lwb, '\''), internal.interface.string.escape(hb, '\''), internal.interface.string.escape(lb, '\'')))
+                    if any(not cls.hexQ(b) for b in {hwb, lwb, hb, lb}):
+                        raise internal.exceptions.InvalidFormatError(u"{:s}.unescape({!s}) : Expected the next four characters ('{:s}', '{:s}', '{:s}', '{:s}') to be hex digits for a unicode character.".format('.'.join(('internal', __name__, cls.__name__)), result, internal.interface.string.escape(hwb, '\''), internal.interface.string.escape(lwb, '\''), internal.interface.string.escape(hb, '\''), internal.interface.string.escape(lb, '\'')))
 
                     # convert the four hex digits into their integral forms
                     hw, lw, h, l = map(cls.of_hex, (hwb.lower(), lwb.lower(), hb.lower(), lb.lower()))
@@ -1243,8 +1243,8 @@ class character(object):
                 # if we find a 'U' prefix, then we have a long unicode character
                 elif t == 'U':
                     Hwb, Lwb, hwb, lwb, hb, lb = (yield), (yield), (yield), (yield), (yield), (yield)
-                    if any(map(cls.hexQ, (Hwb, Lwb, hwb, lwb, hb, lb))) or Hwb not in {'0', '1'}:
-                        raise internal.exceptions.InvalidFormatError(u"{:s}.unescape({!s}) : Expected the next six characters ('{:s}', '{:s}', '{:s}', '{:s}', '{:s}', '{:s}') to be hex digits for a long-unicode character.".format('.'.join(('internal', __name__, cls.__name__)), iterable, internal.interface.string.escape(Hwb, '\''), internal.interface.string.escape(Lwb, '\''), internal.interface.string.escape(hwb, '\''), internal.interface.string.escape(lwb, '\''), internal.interface.string.escape(hb, '\''), internal.interface.string.escape(lb, '\'')))
+                    if any(not cls.hexQ(b) for b in {Hwb, Lwb, hwb, lwb, hb, lb}) or Hwb not in {'0', '1'}:
+                        raise internal.exceptions.InvalidFormatError(u"{:s}.unescape({!s}) : Expected the next six characters ('{:s}', '{:s}', '{:s}', '{:s}', '{:s}', '{:s}') to be hex digits for a long-unicode character.".format('.'.join(('internal', __name__, cls.__name__)), result, internal.interface.string.escape(Hwb, '\''), internal.interface.string.escape(Lwb, '\''), internal.interface.string.escape(hwb, '\''), internal.interface.string.escape(lwb, '\''), internal.interface.string.escape(hb, '\''), internal.interface.string.escape(lb, '\'')))
 
                     # convert the six hex digits into their integral forms
                     Hw, Lw, hw, lw, h, l = map(cls.of_hex, (Hwb.lower(), Lwb.lower(), hwb.lower(), lwb.lower(), hb.lower(), lb.lower()))
@@ -1260,7 +1260,7 @@ class character(object):
                     0))
 
                 else:
-                    raise internal.exceptions.InvalidFormatError(u"{:s}.unescape({!s}) : An unknown character code was specified ('{:s}').".format('.'.join(('internal', __name__, cls.__name__)), iterable, internal.interface.string.escape(t, '\'')))
+                    raise internal.exceptions.InvalidFormatError(u"{:s}.unescape({!s}) : An unknown character code was specified ('{:s}').".format('.'.join(('internal', __name__, cls.__name__)), result, internal.interface.string.escape(t, '\'')))
 
             # we haven't received a backslash, so there's nothing to unescape
             else:
