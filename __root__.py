eb6f721865213081237bceb13b9c548593bae5ec
Ali Rizvi-Santiago <arizvisa@gmail.com>
2020-03-13 15:38:48 -0500

    __root__: swapped the hooks around so the later versions of IDA gets priority rather than the older ones.

--- __root__.py
+++ __root__.py
@@ -105,65 +105,65 @@ ui.hook.ui.add('term', ui.hook.__stop_ida__, 10000)
 
 ## setup default integer types for the typemapper once the loader figures everything out
 if idaapi.__version__ >= 7.0:
-    ui.hook.idp.add('newprc', __import__('internal').interface.typemap.__newprc__, 0)
-else:
     ui.hook.idp.add('ev_newprc', __import__('internal').interface.typemap.__ev_newprc__, 0)
+else:
+    ui.hook.idp.add('newprc', __import__('internal').interface.typemap.__newprc__, 0)
 
 ## monitor when ida enters its various states
 if idaapi.__version__ >= 7.0:
-    ui.hook.idp.add('init', __import__('hooks').on_init, 0)
-    ui.hook.idp.add('newfile', __import__('hooks').on_newfile, 0)
-    ui.hook.idp.add('oldfile', __import__('hooks').on_oldfile, 0)
-    ui.hook.idp.add('auto_empty', __import__('hooks').on_ready, 0)
-else:
     ui.hook.idp.add('ev_init', __import__('hooks').on_init, 0)
     ui.hook.idp.add('ev_newfile', __import__('hooks').on_newfile, 0)
     ui.hook.idp.add('ev_oldfile', __import__('hooks').on_oldfile, 0)
     ui.hook.idp.add('ev_auto_queue_empty', __import__('hooks').auto_queue_empty, 0)
+else:
+    ui.hook.idp.add('init', __import__('hooks').on_init, 0)
+    ui.hook.idp.add('newfile', __import__('hooks').on_newfile, 0)
+    ui.hook.idp.add('oldfile', __import__('hooks').on_oldfile, 0)
+    ui.hook.idp.add('auto_empty', __import__('hooks').on_ready, 0)
 
 ## create the tagcache netnode when a database is created
 if idaapi.__version__ >= 7.0:
-    ui.hook.idp.add('init', __import__('internal').comment.tagging.__init_tagcache__, 0)
-else:
     ui.hook.idp.add('ev_init', __import__('internal').comment.tagging.__init_tagcache__, 0)
+else:
+    ui.hook.idp.add('init', __import__('internal').comment.tagging.__init_tagcache__, 0)
 
 ## hook any user-entered comments so that they will also update the tagcache
 if idaapi.__version__ >= 7.0:
-    [ ui.hook.idb.add(_, __import__('hooks').noapi, 40) for _ in ('changing_cmt', 'cmt_changed', 'changing_area_cmt', 'area_cmt_changed') ]
-else:
     [ ui.hook.idb.add(_, __import__('hooks').noapi, 40) for _ in ('changing_cmt', 'cmt_changed', 'changing_range_cmt', 'range_cmt_changed') ]
+else:
+    [ ui.hook.idb.add(_, __import__('hooks').noapi, 40) for _ in ('changing_cmt', 'cmt_changed', 'changing_area_cmt', 'area_cmt_changed') ]
 
 if idaapi.__version__ >= 7.0:
-    ui.hook.idp.add('init', __import__('hooks').address.database_init, 45)
-    ui.hook.idp.add('init', __import__('hooks').globals.database_init, 45)
-    ui.hook.idb.add('changing_area_cmt', __import__('hooks').globals.changing, 45)
-    ui.hook.idb.add('area_cmt_changed', __import__('hooks').globals.changed, 45)
-else:
     ui.hook.idp.add('ev_init', __import__('hooks').address.database_init, 45)
     ui.hook.idp.add('ev_init', __import__('hooks').globals.database_init, 45)
     ui.hook.idb.add('changing_range_cmt', __import__('hooks').globals.changing, 45)
     ui.hook.idb.add('range_cmt_changed', __import__('hooks').globals.changed, 45)
+else:
+    ui.hook.idp.add('init', __import__('hooks').address.database_init, 45)
+    ui.hook.idp.add('init', __import__('hooks').globals.database_init, 45)
+    ui.hook.idb.add('changing_area_cmt', __import__('hooks').globals.changing, 45)
+    ui.hook.idb.add('area_cmt_changed', __import__('hooks').globals.changed, 45)
 
 ui.hook.idb.add('changing_cmt', __import__('hooks').address.changing, 45)
 ui.hook.idb.add('cmt_changed', __import__('hooks').address.changed, 45)
 
 ## hook naming and "extra" comments to support updating the implicit tags
 if idaapi.__version__ >= 7.0:
-    ui.hook.idp.add('rename', __import__('hooks').rename, 40)
-else:
     ui.hook.idp.add('ev_rename', __import__('hooks').rename, 40)
+else:
+    ui.hook.idp.add('rename', __import__('hooks').rename, 40)
 ui.hook.idb.add('extra_cmt_changed', __import__('hooks').extra_cmt_changed, 40)
 
 ## hook function transformations so we can shuffle their tags between types
 if idaapi.__version__ >= 7.0:
-    ui.hook.idb.add('removing_func_tail', __import__('hooks').removing_func_tail, 40)
-    [ ui.hook.idp.add(_, getattr(__import__('hooks'), _), 40) for _ in ('add_func', 'del_func', 'set_func_start', 'set_func_end') ]
-else:
     ui.hook.idb.add('deleting_func_tail', __import__('hooks').removing_func_tail, 40)
     ui.hook.idb.add('func_added', __import__('hooks').add_func, 40)
     ui.hook.idb.add('deleting_func', __import__('hooks').del_func, 40)
     ui.hook.idb.add('set_func_start', __import__('hooks').set_func_start, 40)
     ui.hook.idb.add('set_func_end', __import__('hooks').set_func_end, 40)
+else:
+    ui.hook.idb.add('removing_func_tail', __import__('hooks').removing_func_tail, 40)
+    [ ui.hook.idp.add(_, getattr(__import__('hooks'), _), 40) for _ in ('add_func', 'del_func', 'set_func_start', 'set_func_end') ]
 [ ui.hook.idb.add(_, getattr(__import__('hooks'), _), 40) for _ in ('thunk_func_created', 'func_tail_appended') ]
 
 ## rebase the entire tagcache when the entire database is rebased.
@@ -171,9 +171,9 @@ ui.hook.idb.add('allsegs_moved', __import__('hooks').rebase, 50)
 
 ## switch the instruction set when the processor is switched
 if idaapi.__version__ >= 7.0:
-    ui.hook.idp.add('newprc', instruction.__newprc__, 50)
-else:
     ui.hook.idp.add('ev_newprc', instruction.__ev_newprc__, 50)
+else:
+    ui.hook.idp.add('newprc', instruction.__newprc__, 50)
 
 ## just some debugging notification hooks
 #[ ui.hook.ui.add(n, __import__('hooks').notify(n), -100) for n in ('range','idcstop','idcstart','suspend','resume','term','ready_to_run') ]
