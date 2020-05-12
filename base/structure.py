4c46f7672aaac4609da61b96398a52e3b13946b5
Ali Rizvi-Santiago <arizvisa@gmail.com>
2020-05-12 14:17:46 -0500

    base/structure: added support to both the `members_t` and `member_t` classes to emit the typeinfo, and fixed the alignment for their fields when displaying them.

--- base/structure.py
+++ base/structure.py
@@ -1207,16 +1207,18 @@ class members_t(object):
     def __repr__(self):
         '''Display all the fields within the specified structure.'''
         res = []
-        mn, ms = 0, 0
+        mn, ms, mti = 0, 0, 0
         for i in six.moves.range(len(self)):
             m = self[i]
-            name, t, ofs, size, comment, tag = m.name, m.type, m.offset, m.size, m.comment, m.tag()
-            res.append((i, name, t, ofs, size, comment or '', tag))
-            mn = max((mn, len(name)))
-            ms = max((ms, len("{:+#x}".format(size))))
-        mi = len("{:d}".format(len(self)))
-        mo = max(map(len, map("{:x}".format, (self.baseoffset, self.baseoffset+self[-1].size))))
-        return "{!r}\n{:s}".format(self.parent, '\n'.join("[{:{:d}d}] {:>{:d}x}{:<+#{:d}x} {:<{:d}s} {!r} {:s}".format(i, mi, o, mo, s, ms, utils.string.repr(n), mn+2, t, " // {!s}".format(utils.string.repr(T) if '\n' in c else c.encode('utf8')) if c else '') for i, n, t, o, s, c, T in res))
+            name, t, ti, ofs, size, comment, tag = m.name, m.type, m.typeinfo, m.offset, m.size, m.comment, m.tag()
+            res.append((i, name, t, ti, ofs, size, comment or '', tag))
+            mn = max(mn, len(name))
+            ms = max(ms, len("{:+#x}".format(size)))
+            mti = max(mti, len("{!s}".format(ti).replace(' *', '*')))
+        mi = len("{:d}".format(len(self) - 1)) if len(self) else 1
+        mo = max(map(len, map("{:x}".format, (self.baseoffset, self[-1].offset + self[-1].size))))
+        print("{:x}".format(self.baseoffset + self[-1].size))
+        return "{!r}\n{:s}".format(self.parent, '\n'.join("[{:{:d}d}] {:>{:d}x}{:<+#{:d}x} {:>{:d}s} {:<{:d}s} {!s} {:s}".format(i, mi, o, mo, s, ms, "{!s}".format(ti).replace(' *','*'), mti, utils.string.repr(n), mn+2, utils.string.repr(t), " // {!s}".format(utils.string.repr(T) if '\n' in c else c.encode('utf8')) if c else '') for i, n, t, ti, o, s, c, T in res))
 
 class member_t(object):
     """
@@ -1541,8 +1543,8 @@ class member_t(object):
 
     def __repr__(self):
         '''Display the member in a readable format.'''
-        id, name, typ, comment, tag = self.id, self.fullname, self.type, self.comment or '', self.tag()
-        return "<member '{:s}' index={:d} offset={:-#x} size={:+#x}> {!s}{:s}".format(utils.string.escape(name, '\''), self.index, self.offset, self.size, utils.string.repr(typ), " // {!s}".format(utils.string.repr(tag) if '\n' in comment else comment.encode('utf8')) if comment else '')
+        id, name, typ, comment, tag, typeinfo = self.id, self.fullname, self.type, self.comment or '', self.tag(), "{!s}".format(self.typeinfo).replace(' *', '*')
+        return "<member '{:s}' index={:d} offset={:-#x} size={:+#x}{:s}> {:s}".format(utils.string.escape(name, '\''), self.index, self.offset, self.size, " typeinfo='{:s}'".format(typeinfo) if len("{:s}".format(typeinfo)) else '', " // {!s}".format(utils.string.repr(tag) if '\n' in comment else comment.encode('utf8')) if comment else '')
 
     def refs(self):
         """Return the `(address, opnum, type)` of all the code and data references to this member within the database.
