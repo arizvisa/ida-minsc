c35be82b80d56ebd7bc55b8eb89c069dc6402a44
Ali Rizvi-Santiago <arizvisa@gmail.com>
2021-10-07 19:03:21 -0500

    base/instruction: corrected the dtype that's used when defining some of the registers in the `Intel` architecture.

--- base/instruction.py
+++ base/instruction.py
@@ -2942,7 +2942,7 @@ class Intel(interface.architecture_t):
 
         # sse1/sse2 simd registers
         [ setitem("xmm{:d}".format(_), self.new("xmm{:d}".format(_), 128, dtype=idaapi.dt_byte16)) for _ in range(16) ]
-        [ setitem("ymm{:d}".format(_), self.new("ymm{:d}".format(_), 128, dtype=idaapi.dt_ldbl)) for _ in range(16) ]
+        [ setitem("ymm{:d}".format(_), self.new("ymm{:d}".format(_), 256, dtype=idaapi.dt_byte32)) for _ in range(16) ]
 
         # control registers
         [ setitem("cr{:d}".format(_), self.new("cr{:d}".format(_), database.config.bits())) for _ in range(8) ]
