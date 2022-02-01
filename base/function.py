3db8b28ad29c3b3526f95899dbc7b44586694347
Ali Rizvi-Santiago <arizvisa@gmail.com>
2022-02-01 17:58:39 -0600

    base/function: added a workaround to the `chunk.owners` function to guard against versions of IDAPython where the referrers for a tail is undefined.

--- base/function.py
+++ base/function.py
@@ -547,24 +547,46 @@ class chunk(object):
     @classmethod
     def owners(cls, ea):
         '''Yield each of the owners which have the function chunk containing the address `ea` associated with it.'''
-        ch = idaapi.get_fchunk(ea)
+        fn = idaapi.get_func(ea)
+
+        # If we're not associated with a function, then we just leave. Otherwise,
+        # we grab the function chunk for the requested address.
+        if fn is None:
+            return
 
         # If we were unable to get the function chunk for the provided address,
         # then we can just return because there's nothing that owns it.
+        ch = idaapi.get_fchunk(ea)
         if ch is None:
-            return
+            raise internal.exceptions.DisassemblerError(u"{:s}.owners({:#x}) : Unable to read the chunk belonging to the function at {:#x}.".format('.'.join([__name__, cls.__name__]), ea, internal.interface.range.start(fn)))
+        bounds = internal.interface.range.bounds(ch)
 
         # If this is a function tail, then we need to iterate through the referers
         # for the chunk so that we can yield each address. Older versions of IDA
         # don't always give us an array, so we construct it if we don't get one.
-        if ch.flags & idaapi.FUNC_TAIL and ch.refqty > 0:
-            tids = idaapi.tid_array(ch.refqty)
-            referers = ch.referers if hasattr(ch.referers, 'count') else tids.frompointer(ch.referers)
-            iterable = (referers[index] for index in builtins.range(ch.refqty))
+        if ch.flags & idaapi.FUNC_TAIL:
+            count, tids = ch.refqty, idaapi.tid_array(ch.refqty)
+
+            # If the referers are None, then our chunk might be busted
+            # because IDAPython is fucking garbage. If there's only
+            # more than one referrer, then we need to fail here.
+            if ch.referers is None and count > 1:
+                raise internal.exceptions.DisassemblerError(u"{:s}.owners({:#x}) : Unable to read more than one referrer ({:d}) for the function tail at {!s}. The \"{:s}\" attribute is {!s}.".format('.'.join([__name__, cls.__name__]), ea, count, bounds, 'referers', ch.referers))
+
+            # Otherwise, we can handle this by using the function that
+            # we snagged in order to determine the owner of the address.
+            elif ch.referers is None:
+                referers = [ internal.interface.range.start(fn) ]
+
+            # Our referers are normal, so we can simply convert them
+            # into an iterable so that they get yielded later.
+            else:
+                referers = ch.referers if hasattr(ch.referers, 'count') else tids.frompointer(ch.referers)
+            iterable = (referers[index] for index in builtins.range(count))
 
         # Otherwise, we just need to yield the function that owns this chunk.
         else:
-            iterable = (ea for ea, _ in map(interface.range.bounds, [ch]))
+            iterable = (ea for ea, _ in [bounds])
 
         # We've collected all of our items, so iterate through what we've collected
         # and then yield them to the caller before returning.
