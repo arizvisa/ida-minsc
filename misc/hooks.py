2404ea2517077ff792e09cc5c06571579d62ee0d
Ali Rizvi-Santiago <arizvisa@gmail.com>
2022-02-16 00:31:59 -0600

    misc/hooks: corrected the name of the method that is used when disconnecting the notification hooks upon shutdown.

--- misc/hooks.py
+++ misc/hooks.py
@@ -1961,7 +1961,7 @@ def make_ida_not_suck_cocks(nw_code):
 
 def make_ida_suck_cocks(nw_code):
     '''Unhook all of IDA's API.'''
-    idaapi.__notification__.unhook()
+    idaapi.__notification__.close()
     ui.hook.__stop_ida__()
 
 def ida_is_busy_sucking_cocks(*args, **kwargs):
