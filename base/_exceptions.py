b3bc8d6cfd60951bf1649e7ddad4881d70c64235
Ali Rizvi-Santiago <arizvisa@gmail.com>
2022-08-19 15:34:17 -0500

    internal/exceptions: fixed the exceptions so that they always assign their required attributes despite multiple-inheritance.

--- base/_exceptions.py
+++ base/_exceptions.py
@@ -100,151 +100,241 @@ class MissingTagError(E.KeyError, UnicodeException):
     """
     The requested tag at the specified address does not exist.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class MissingFunctionTagError(MissingTagError):
     """
     The requested tag for the specified function does not exist.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class MissingMethodError(E.NotImplementedError, UnicodeException):
     """
     A method belonging to a superclass that is required to be overloaded was called.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class MissingNameError(E.NameError, UnicodeException):
     """
     A name that was required was found missing and was unable to be recovered.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class UnsupportedVersion(E.NotImplementedError, UnicodeException):
     """
     This functionality is not supported on the current version of IDA.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class UnsupportedCapability(E.NotImplementedError, E.EnvironmentError, UnicodeException):
     """
     An unexpected or unsupported capability was specified.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class ResultMissingError(E.LookupError, UnicodeException):
     """
     The requested item is missing from its results.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class SearchResultsError(ResultMissingError):
     """
     No results were found.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class DisassemblerError(E.EnvironmentError, UnicodeException):
     """
     An api call has thrown an error or was unsuccessful.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class MissingTypeOrAttribute(E.TypeError, UnicodeException):
     """
     The specified location is missing some specific attribute or type.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class InvalidTypeOrValueError(E.TypeError, E.ValueError, UnicodeException):
     """
     An invalid value or type was specified.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class InvalidParameterError(E.AssertionError, InvalidTypeOrValueError):
     """
     An invalid parameter was specified by the user.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class OutOfBoundsError(E.ValueError, UnicodeException):
     """
     The specified item is out of bounds.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class AddressOutOfBoundsError(E.ArithmeticError, OutOfBoundsError):
     """
     The specified address is out of bounds.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class IndexOutOfBoundsError(E.IndexError, E.KeyError, OutOfBoundsError):
     """
     The specified index is out of bounds.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class ItemNotFoundError(E.KeyError, ResultMissingError):
     """
     The specified item or type was not found.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class FunctionNotFoundError(ItemNotFoundError):
     """
     Unable to locate the specified function.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class AddressNotFoundError(ItemNotFoundError):
     """
     Unable to locate the specified address.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class SegmentNotFoundError(ItemNotFoundError):
     """
     Unable to locate the specified segment.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class StructureNotFoundError(ItemNotFoundError):
     """
     Unable to locate the specified structure.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class EnumerationNotFoundError(ItemNotFoundError):
     """
     Unable to locate the specified enumeration.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class MemberNotFoundError(ItemNotFoundError):
     """
     Unable to locate the specified structure or enumeration member.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class RegisterNotFoundError(ItemNotFoundError):
     """
     Unable to locate the specified register.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class NetNodeNotFoundError(ItemNotFoundError):
     """
     Unable to locate the specified netnode.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class ReadOrWriteError(E.IOError, E.ValueError, UnicodeException):
     """
     Unable to read or write the specified number of bytes .
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class InvalidFormatError(E.KeyError, E.ValueError, UnicodeException):
     """
     The specified data has an invalid format.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class SerializationError(E.ValueError, E.IOError, UnicodeException):
     """
     There was an error while trying to serialize or deserialize the specified data.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class SizeMismatchError(SerializationError):
     """
     There was an error while trying to serialize or deserialize the specified data due to its size not matching.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class UnknownPrototypeError(E.LookupError, UnicodeException):
     """
     The requested prototype does not match any of the ones that are available.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 class DuplicateItemError(E.NameError, UnicodeException):
     """
     The requested command has failed due to a duplicate item.
     """
+    def __init__(self, *args):
+        self.__args__ = args
+        self.__message__ = args[0] if len(args) == 1 else ''
 
 #structure:742 and previous to it should output the module name, classname, and method
 #comment:334 should catch whatever tree.find raises
