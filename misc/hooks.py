2c6a75a9e471eaca0fb2d62cb44c2397439696e1
Ali Rizvi-Santiago <arizvisa@gmail.com>
2026-07-31 14:47:07 -0500

    internal/hooks: updated the `entrymethods` definitions for `IDB_Hooks.struc_renamed`.

--- misc/hooks.py
+++ misc/hooks.py
@@ -6820,11 +6820,16 @@ class entrymethods(object):
         def bookmark_changed(self, index, pos, desc, operation):
             '''introduced by commit c22e07185389adaef1698314071f2c6cd31dd08b to patch_codegen/idp.py:69.'''
 
+        # idaapi.__version__ >= 8.0
+        def struc_renamed(self, sptr, success):
+            '''introduced by commit 023c5ba53ca91c730d39539c2c50778ac8e5cd87 to patch_codegen/idp.py:87.'''
+
         idaapi.__version__ >= 7.6 and mapping.setdefault('compiler_changed', compiler_changed)
         idaapi.__version__ < 8.4 and mapping.setdefault('local_types_changed', busted_local_types_changed)
         idaapi.__version__ >= 8.4 and mapping.setdefault('local_types_changed', local_types_changed)
         idaapi.__version__ >= 7.7 and mapping.setdefault('segm_deleted', segm_deleted)
         idaapi.__version__ >= 7.6 and mapping.setdefault('bookmark_changed', bookmark_changed)
+        idaapi.__version__ >= 8.0 and mapping.setdefault('struc_renamed', struc_renamed)
 
     class IDP_Hooks(object):
         """
