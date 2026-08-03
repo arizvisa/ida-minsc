ba36ecca4b8a3cfcabd0569756c48aed223ec8fd
Ali Rizvi-Santiago <arizvisa@gmail.com>
2026-08-03 18:43:18 -0500

    internal/interface: tweaked the `contiguous.layout` function to split out the type checking.

--- misc/interface.py
+++ misc/interface.py
@@ -7124,14 +7124,34 @@ class contiguous(object):
             for item, original in items:
                 res = size[item.__class__](item)
                 offset += direction * res
-                yield math.trunc(offset), original if isinstance(item, internal.types.integer) and isinstance(original, symbol_t) else original if isinstance(original, (internal.types.list, internal.types.tuple)) or (getattr(original, '__hash__', None) and original in typemap.typemap) else item
+
+                if isinstance(item, internal.types.integer) and isinstance(original, symbol_t):
+                    newitem = original
+                elif isinstance(original, (internal.types.list, internal.types.tuple)):
+                    newitem = original
+                elif getattr(original, '__hash__', None) and original in typemap.typemap:
+                    newitem = original
+                else:
+                    newitem = item
+
+                yield math.trunc(offset), newitem
             return
 
         # Otherwise, we start at the current offset, and
         # adjust the offset for each item as it comes in.
         for item, original in items:
             res = size[item.__class__](item)
-            yield math.trunc(offset), original if isinstance(item, internal.types.integer) and isinstance(original, symbol_t) else original if isinstance(original, (internal.types.list, internal.types.tuple)) or (getattr(original, '__hash__', None) and original in typemap.typemap) else item
+
+            if isinstance(item, internal.types.integer) and isinstance(original, symbol_t):
+                newitem = original
+            elif isinstance(original, (internal.types.list, internal.types.tuple)):
+                newitem = original
+            elif getattr(original, '__hash__', None) and original in typemap.typemap:
+                newitem = original
+            else:
+                newitem = item
+
+            yield math.trunc(offset), newitem
             offset += direction * res
         return
 
