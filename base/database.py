de7c4d2868691ae93c00a02250b3274e5a53ded2
Ali Rizvi-Santiago <arizvisa@gmail.com>
2026-09-01 00:07:54 -0500

    base/database: updated the `tag` function to return a "__name__" tag if a name exists.

--- base/database.py
+++ base/database.py
@@ -2296,9 +2296,9 @@ def tag(ea, key):
     elif key not in {'__name__'}:
         raise E.MissingTagError(u"{:s}.tag({:#x}, {!r}) : Unable to read tag (\"{:s}\") from address.".format(__name__, ea, key, utils.string.escape(key, '"')))
     elif not interface.function.has(ea):
-        return internal.tags.address.name(ea, False)
+        return internal.tags.address.name(ea, False) if interface.name.has(ea) else None
     entrypoint = interface.range.start(interface.function.by_address(ea))
-    return internal.tags.address.name(ea, entrypoint == ea)
+    return internal.tags.address.name(ea, entrypoint == ea) if interface.name.has(ea) else None
 @utils.multicase(ea=internal.types.integer, key=internal.types.string)
 @utils.string.decorate_arguments('key', 'value')
 def tag(ea, key, value):
